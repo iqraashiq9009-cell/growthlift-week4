5d89447828a46bba1f4d357da0325c48a96c7c8d
Iqra Ashiq <iqra.ashiq.9009@gmail.com>
2026-07-20 19:13:24 +0500

    Day 17: Express routing, params, middleware

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.end("Hello from my server!");
+const PORT = 3000;
+
+// Middleware - logs every request
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
 });
 
-server.listen(3000, () => console.log("Server running on port 3000"));
+// Middleware - allows reading JSON in request bodies
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to GrowthLift API");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is the About route");
+});
+
+app.get("/api/interns", (req, res) => {
+  res.json({ interns: ["Ali", "Sara", "Bilal"] });
+});
+
+app.get("/api/interns/:id", (req, res) => {
+  res.json({ id: req.params.id, name: "Sample Intern" });
+});
+
+app.get("/api/search", (req, res) => {
+  res.json({ query: req.query.q });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
