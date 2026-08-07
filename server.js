c464ebfd05913115d21b69f07060b64b88ffadc0
Iqra Ashiq <iqra.ashiq.9009@gmail.com>
2026-08-07 14:08:04 +0500

    fix: use dynamic PORT for deployment

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const protect = require("./middleware/auth");
 app.use(helmet());
 app.use(cors());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
